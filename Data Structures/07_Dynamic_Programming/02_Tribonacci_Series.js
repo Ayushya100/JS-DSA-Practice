6a47b8e6ff0227608f90ab0a28b5d25de1034bb7
Ayushya Jaiswal <56911125+Ayushya100@users.noreply.github.com>
2024-10-22 07:22:06 +0000

    Tribonacci Series using Dynamic Programming

---
+++ Data Structures/07_Dynamic_Programming/02_Tribonacci_Series.js
@@ -0,0 +1,33 @@
+// Memoization
+const tribMemo = (n, memo = {}) => {
+    if (n in memo) {
+        return memo[n];
+    }
+
+    if (n === 0) {
+        return 0
+    } else if (n === 1 || n === 2) {
+        return 1;
+    }
+
+    memo[n] = tribMemo(n - 1, memo) + tribMemo(n - 2, memo) + tribMemo(n - 3, memo);
+    return memo[n];
+}
+
+// Tabulation
+const tribTable = (n) => {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1 || n === 2) {
+        return 1;
+    }
+
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+    table[2] = 1;
+
+    for (let i = 3; i <= n; i++) {
+        table[i] = table[i - 1] + table[i - 2] + table[i - 3];
+    }
+    return table[n];
+}
