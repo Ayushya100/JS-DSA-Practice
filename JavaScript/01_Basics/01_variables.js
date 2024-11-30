d6b0bcc0999ad48bcd1b621c0d8f48d588054de4
Ayushya Jaiswal <56911125+Ayushya100@users.noreply.github.com>
2024-11-30 06:59:59 +0000

    Variables in JS

---
+++ JavaScript/01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1235512;
+let accountEmail = 'jaiswalayushya@gmail.com';
+var accountPassword = '818111';
+accountCity = 'Bangalore';
+
+accountEmail = 'jaiswalayushya296@gmail.com';
+accountPassword = '12111';
+accountCity = 'Prayagraj';
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
