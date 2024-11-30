85aeca8c20125e01d1c7092e79b201c3fd7b0232
Ayushya Jaiswal <56911125+Ayushya100@users.noreply.github.com>
2024-11-30 08:46:15 +0000

    conversion operations in JS

--- JavaScript/01_Basics/03_conversionOperation.js
+++ JavaScript/01_Basics/03_conversionOperation.js
@@ -26,3 +26,22 @@ let numberToString = String(stringNumber);
 console.log(typeof numberToString);
 console.log(numberToString);
 
+// ------------------------------------------------------- Operations -------------------------------------------------
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = 'Hello';
+let str2 = ' World';
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log('1' + 2);       // 12
+console.log(1 + '2');       // 12
+console.log('1' + 2 + 2);   // 122
+console.log(1 + 2 + '2');   //32
+
+console.log(+true);     // 1
+console.log(+'');       // 0
+
