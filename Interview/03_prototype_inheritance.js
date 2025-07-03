38e89a78c6ae702870effd951c6b523e76a7ff16
Ayushya Jaiswal <56911125+Ayushya100@users.noreply.github.com>
2025-07-03 04:19:57 +0000

    Added prototype inheritance Interview questions

---
+++ Interview/03_prototype_inheritance.js
@@ -0,0 +1,71 @@
+const company = {
+    name: 'ABC inc.',
+    salary: function() {
+        console.log(`500k Salary`);
+    }
+};
+
+
+// Interview Question - 1 - __proto__
+const engineer1 = {
+    empId: 123,
+    task: function() {
+        console.log('Task assigned');
+    }
+};
+
+engineer1.__proto__ = company;
+console.log(engineer1);
+engineer1.task();
+engineer1.salary();
+
+
+// Interview Question - 2 - Object.create
+const engineer2 = Object.create(company, {
+    empId: { value: 456 },
+    task: {
+        value: function() {
+            console.log('Task assigned');
+        }
+    }
+});
+
+console.log(engineer2);
+engineer2.task();
+engineer2.salary();
+
+
+// Interview Question - 3 - setPrototypeOf
+const engineer3 = {
+    empId: 789,
+    task: function() {
+        console.log('Task assigned');
+    }
+};
+
+Object.setPrototypeOf(engineer3, company);
+console.log(engineer3);
+engineer3.task();
+engineer3.salary();
+
+
+// Interview Question - 4 - Multiple Prototypal Inheritance
+const orgainization = {
+    orgName: 'Tech Group',
+    location: 'Global',
+    info: function() {
+        console.log(`Organization: ${this.orgName}, Location: ${this.location}`);
+    }
+}
+
+Object.setPrototypeOf(company, orgainization);
+
+console.log(engineer3);
+engineer3.task();
+engineer3.salary();
+engineer3.info();
+
+
+// Interview Question - 5 - getPrototypeOf
+const prototypeList = Object.getPrototypeOf(engineer3);
+console.log(prototypeList);
